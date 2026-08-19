de1ae59a6482983cd8d68180bd1ba34f6c862950
Tushar kumar <narutoboi41@gmail.com>
2026-08-19 13:31:38 +0530

    deploy

--- Server/src/app.js
+++ Server/src/app.js
@@ -10,7 +10,7 @@ const cors = require("cors")
 
 
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://task-manager-frontend-iogv.onrender.com",
     credentials : true
 }))
 
